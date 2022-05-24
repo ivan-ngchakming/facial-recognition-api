a76f7d0a8bb398eb894d3de7e553a5d9eda3c4f2
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-05-24 13:19:39 +0800

    configure test workflow

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,12 @@
 from server import create_app
 import json
 
+from server.config import TestingConfig
+
 
 def test_config():
     assert not create_app().testing
-    assert create_app({"TESTING": True}).testing
+    assert create_app(TestingConfig).testing
 
 
 def test_index(client, app):
